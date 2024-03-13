4fb89e606d191e71cb2868207c03422368c4febf
Yanis540 <yanistabellout4@gmail.com>
2024-03-13 11:00:52 +0100

    [APS1A] : added base code

---
+++ APS1A/checker.pl
@@ -0,0 +1,183 @@
+
+/******************************* UTILS ********************************/
+/* insipré de : https://github.com/valeeraZ/Sorbonne_APS/blob/master/APS0/typeChecker.pl (on utilise pas leurs code plus qu'il n'est pas adapté à l'énoncé ) */
+parcours(X, [(X,V)|_], V).
+parcours(X, [_|XS], V) :- parcours(X, XS, V).
+
+get_type([],[]).
+get_type([A|ARGS],[T|TYPES]) :-
+	type_expr([],A,T),
+	get_type(ARGS,TYPES).
+
+get_type_args([],[]).
+get_type_args([(_,T)|ARGS],[T|RES]) :-
+	get_type_args(ARGS,RES).
+		
+verifier_arguments(_,[],[]).
+verifier_arguments(G,[ARG|ARGS],[ARGTYPE|ARGSTYPE]) :-
+	type_expr(G,ARG,ARGTYPE),
+	verifier_arguments(G,ARGS,ARGSTYPE).
+
+/******************************* UTILS ********************************/
+/* g0 */
+g0([
+    (false, bool),
+    (true, bool),
+    (not, typeFunc([bool],bool)),
+    (eq, typeFunc([int,int],bool)),
+    (lt, typeFunc([int,int],bool)),
+    (add, typeFunc([int,int],int)),
+    (sub, typeFunc([int,int],int)),
+    (mul, typeFunc([int,int],int)),
+    (div, typeFunc([int,int],int))
+]).
+
+/******************************* EXPRESSIONS ********************************/
+
+/* Num */
+type_expr(_,num(N),int) :-
+ 	integer(N).
+
+/* id */
+type_expr(G,id(IDF),T):-
+    parcours(IDF,G,T).
+
+/* IF */
+type_expr(G,if(E1,E2,E3),T) :- 
+    type_expr(G,E1,bool),
+    type_expr(G,E2,T),
+    type_expr(G,E3,T).
+
+/* lambda : [ARGS] e (abs) */
+type_expr(G,lambda(ARGS,E),typeFunc(RES,T)):- 
+    append(ARGS,G,GU), 
+    get_type_args(ARGS,RES),
+    type_expr(GU,E,T).
+/* app : e e1 e2 e3 ... 
+  - vérifier que e est bien une fonction de type : (t1*t2*....*tn) -> T 
+  - vérifier que : e1:t1 et e2:t2 ... en:fn 
+  e.g : g0(L),type_expr(L,app(id(add),[num(1),num(2)]),T).
+*/
+type_expr(G,app(E,ARGS),T):- 
+    type_expr(G,E,typeFunc(ARGSTYPE,T)),
+    verifier_arguments(G,ARGS,ARGSTYPE).
+
+
+/* and 
+    e.g : g0(L),type_expr(L,and(id(true),id(false)),T).
+*/
+type_expr(G,and(L,R),bool):-
+    type_expr(G,L,bool),
+    type_expr(G,R,bool).
+/* or 
+    e.g : g0(L),type_expr(L,or(id(true),id(false)),T).
+*/
+type_expr(G,or(L,R),bool):-
+    type_expr(G,L,bool),
+    type_expr(G,R,bool).
+
+/******************************* INSTRUCTIONS ********************************/
+
+
+/* echo */
+type_stat(G,echo(E),void) :-
+	type_expr(G,E,int).
+/* SET */
+type_stat(G,set(id(VAR),E),void) :-
+    /*chercher le type de VAR dans l'environnement puis vérifier */
+    type_expr(G,id(VAR),T),
+    type_expr(G,E,T).
+/* IF */
+type_stat(G,if(E,B_CONS,B_ALT),void) :-
+    type_expr(G,E,bool),
+    type_block(G,B_CONS,void),
+    type_block(G,B_ALT,void).
+/* While */
+type_stat(G,while(C,B),void) :-
+    type_expr(G,C,bool),
+    type_block(G,B,void).
+/* Call */
+type_stat(G,call(X,ARGS),void) :-
+    type_expr(G,X,typeFunc(ARGSTYPE,void)),
+    verifier_arguments(G,ARGS,ARGSTYPE).
+/******************************* DEFINITIONS ********************************/
+/* const */
+type_def(G,constant(X,T,E),[(X,T)|G]):-
+	type_expr(G,E,T).	
+/* func : FUN <NAME> <>
+   - rajouter les arguments dans un env temporaire
+   - évaluer le body ou l'expression relier à la fonction 
+   - mettre à jours les types des arguments 
+   - créer un nouvel environnement gamma' permettant d'enrichir le context gamma 
+    e.g : type_def([],function(yanis,T,[(x,int)],num(1)),G).
+ */
+type_def(G,function(FUNC,T,ARGUMENTS,E),GU):-
+	append(ARGUMENTS,G,G_TEMP),
+	type_expr(G_TEMP,E,T),
+	get_type_args(ARGUMENTS,RES),
+	GU=[(FUNC,typeFunc(RES,T))|G]. 
+/* funcRec : FUNRec <NAME> <>
+   - rajouter les arguments dans un env temporaire
+   - rajouter la définiton de la fonction elle même dans l'env temporaire
+   - évaluer le body ou l'expression relier à la fonction 
+   - mettre à jours les types des arguments 
+   - créer un nouvel environnement gamma' permettant d'enrichir le context gamma 
+    e.g : type_def([],functionRec(yanis,T,[(n,int)],app(id(yanis),[id(n)])),G).
+ */
+type_def(G,functionRec(FUNC,T,ARGUMENTS,E),GU):-
+	get_type_args(ARGUMENTS,RES),
+	append(ARGUMENTS,G,G_TEMP), 
+    G_TEMP_TEMP =[(FUNC,typeFunc(RES,T))|G_TEMP],
+	type_expr(G_TEMP_TEMP,E,T),
+	GU=[(FUNC,typeFunc(RES,T))|G]. 
+/* VAR : */
+type_def(G,var(X,T),[(X,T)|G]).
+
+/* Proc */
+type_def(G,proc(PROC,ARGUMENTS,B),GU):-
+    append(ARGUMENTS,G,G_TEMP),
+	type_block(G_TEMP,B,void),
+	get_type_args(ARGUMENTS,RES),
+	GU=[(PROC,typeFunc(RES,void))|G].
+/* PROC REC */ 
+type_def(G,procRec(PROC,ARGUMENTS,B),GU):-
+    get_type_args(ARGUMENTS,RES),
+	append(ARGUMENTS,G,G_TEMP), 
+    G_TEMP_TEMP =[(PROC,typeFunc(RES,T))|G_TEMP],
+	type_block(G_TEMP_TEMP,B,T),
+	GU=[(PROC,typeFunc(RES,T))|G]. 
+
+/******************************* CMDS ********************************/
+/* defs 
+    e.g : type_cmds([],[declaration(constant(yanis,int,num(1)))],G)
+    e.g : type_cmds([],[declaration(constant(x,T,num(1))),echo(id(x))],G).
+*/
+type_cmds(G,[declaration(X)|Y],void) :-
+	type_def(G,X,G_TEMP),
+	type_cmds(G_TEMP,Y,void).
+
+/* end */
+type_cmds(_,[],void).
+type_cmds(G,[X|Y],void) :-
+	type_stat(G,X,void), 
+	type_cmds(G,Y,void).
+
+
+/******************************* BLOCK ********************************/
+
+type_block(G,block(B),void):-
+    type_cmds(G,B,void).
+
+/******************************* PROG ********************************/
+type_prog(G,prog(X),void) :- type_block(G,X,void).
+
+/* d:-a,f(X) veut dire : d est vrai ssi si a est vrai et f(X) est vrai*/   
+
+
+
+
+main_stdin :-
+	read(user_input,T),
+    g0(L),
+	type_prog(L,T,R),
+	print(R).
---
+++ APS1A/parser.mly
@@ -0,0 +1,122 @@
+%{
+(* ========================================================================== *)
+(* == UPMC/master/info/4I506 -- Janvier 2016/2017                          == *)
+(* == SU/FSI/master/info/MU4IN503 -- Janvier 2020/2021/2022                == *)
+(* == Analyse des programmes et sémantiques                                == *)
+(* ========================================================================== *)
+(* == hello-APS Syntaxe ML                                                 == *)
+(* == Fichier: parser.mly                                                  == *)
+(* == Analyse syntaxique                                                   == *)
+(* ========================================================================== *)
+
+open Ast
+
+%}
+  
+%token <int> NUM
+%token <string> IDENT
+%token LPAR RPAR 
+%token LBRA RBRA
+%token ECHO
+%token BOOL
+%token INT
+%token IF
+%token AND
+%token OR
+%token FUN REC
+%token FUN REC
+%token ARROW STAR SEMICOLON
+%token COLON COMMA
+%token CONST
+%token VAR
+%token PROC
+%token SET
+%token WHILE
+%token CALL
+%token VOID
+
+%type <Ast.expr> expr
+%type <Ast.expr list> exprs
+%type <Ast.cmds> cmds
+%type <Ast.block> block
+%type <Ast.block> prog
+%type <Ast.typ> typ
+%type <Ast.tprim> tprim
+%type <Ast.arg> arg
+%type <Ast.args> args
+
+%start prog
+
+%%
+
+prog : block { $1 };
+
+block: LBRA cmds RBRA    { ASTblock $2 }
+;
+
+cmds:
+  stat                  { ASTStat $1 }
+  | def SEMICOLON cmds {ASTdef($1,$3)}
+  | stat SEMICOLON cmds {ASTstatCmds($1,$3)}
+;
+
+
+def :
+  CONST IDENT typ expr {ASTconst($2,$3,$4)}
+  | FUN IDENT  typ LBRA args RBRA expr {ASTfunc($2,$3,$5,$7)}
+  | FUN REC IDENT  typ LBRA args RBRA expr {ASTfuncRec($3,$4,$6,$8)}
+  | VAR IDENT typ  {ASTvar($2,$3)}
+  | PROC IDENT LBRA args RBRA block {ASTproc($2,$4,$6)}
+  | PROC REC IDENT LBRA args RBRA block {ASTprocRec($3,$5,$7)}
+  
+; 
+
+stat:
+  ECHO expr             { ASTEcho($2) }
+  | SET IDENT expr{ASTset($2,$3)}
+  | IF expr block block{ASTif($2,$3,$4)}
+  | WHILE expr block{ASTwhile($2,$3)}
+  | CALL IDENT exprs {ASTcall($2,$3)}
+;
+
+expr:
+  NUM                   { ASTNum($1) }
+| IDENT                 { ASTId($1) }
+| LPAR IF expr expr expr RPAR { ASTif($3,$4,$5) }
+| LPAR AND expr expr RPAR { ASTand($3,$4) }
+| LPAR OR expr expr RPAR { ASTor($3,$4) }
+| LBRA args RBRA expr  { ASTlambda($2,$4) }
+| LPAR expr exprs RPAR  { ASTApp($2, $3) }
+;
+
+exprs :  
+  expr          { [$1] }
+| expr exprs { $1 :: $2 }
+;
+
+// https://github.com/valeeraZ/Sorbonne_APS/blob/master/APS0/parser.mly
+// : On a utilisé les types primitive ainisi que les types de ce repo 
+// ARGS on l'a fait nous même plus que ct assez simple 
+tprim:
+  INT                       { Int }
+| BOOL                      { Bool }
+| VOID                      { Void }
+;
+
+typ:
+  tprim                     { Type($1) }
+| LPAR types ARROW typ RPAR { TypeFunc($2,$4) }
+;
+
+types:
+  typ                      { [$1] }
+| typ STAR types           { $1::$3 }
+;
+
+arg: IDENT COLON typ {Argument($1,$3)}
+; 
+args :
+  arg { [$1] }
+| arg COMMA args { $1 :: $3 }
+
+;
