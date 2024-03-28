51c3a08f35304e4276c1a8961d5ee98019354134
Yanis540 <yanistabellout4@gmail.com>
2024-03-28 20:23:53 +0100

    [APS1] : continued expected results

---
+++ APS1/archive/SetupExpectedFiles.py
@@ -0,0 +1,29 @@
+import shutil,os
+
+EXPECTED_RESULTS_DIR = './expected'
+RESULTS_DIR = './results'
+SAMPLES_DIR = './Samples'
+SUB_DIRS =['cmds','defs','exprs','prim','stat']
+
+source_dir = "../APS0/expected"
+destination_dir = EXPECTED_RESULTS_DIR
+shutil.copytree(source_dir, destination_dir)
+
+
+for dir in SUB_DIRS : 
+    if not os.path.exists(f'{EXPECTED_RESULTS_DIR}/{dir}'):
+        os.makedirs(f'{EXPECTED_RESULTS_DIR}/{dir}')
+        
+
+for dir in SUB_DIRS : 
+    for file in os.listdir(f'{SAMPLES_DIR}/{dir}') :
+        file_name = file.split('.')[0] 
+        expected_result_dir =f'{EXPECTED_RESULTS_DIR}/{dir}/{file_name}'
+        print(expected_result_dir,os.path.exists(expected_result_dir))
+        if os.path.exists(expected_result_dir):
+            continue
+        os.makedirs(expected_result_dir)
+        expected_type_result_file_path = f'{expected_result_dir}/type.result'
+        expected_eval_result_file_path = f'{expected_result_dir}/eval.result'
+        f=open(expected_type_result_file_path,'w')
+        f=open(expected_eval_result_file_path,'w')
